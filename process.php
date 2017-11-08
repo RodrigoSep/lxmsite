549329ad834d30473ce86fc4ecbd55a75ffdcc4a
RodrigoSep <33297258+RodrigoSep@users.noreply.github.com>
2017-11-08 17:12:03 -0600

    Update process.php
    Testing functionality

--- process.php
+++ process.php
@@ -1,6 +1,6 @@
 <?php
  
-    $to = "carlosgzz93@gmail.com"; 
+    $to = "r.sepulveda.ayala@gmail.com"; 
     $from = $_REQUEST['email']; 
     $name = $_REQUEST['name']; 
     $headers = "From: $from"; 
